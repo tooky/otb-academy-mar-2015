83be194d9ade2c58c3f5cab8b39cf66491dc3317
Christopher Rigby <christopher.rigby@onthebeach.co.uk>
2015-03-02 11:33:59 +0000

    Testing Array

--- the-language/spec/arrays_spec.rb
+++ the-language/spec/arrays_spec.rb
@@ -2,8 +2,8 @@ RSpec.describe "Ruby Arrays" do
   it "can be created like any other object" do
     empty = Array.new
 
-    expect( empty.class ).to eq __
-    expect( empty.size ).to eq __
+    expect( empty.class ).to eq Array
+    expect( empty.size ).to eq 0
   end
 
   it "can also be created using an array literal" do
@@ -13,56 +13,56 @@ RSpec.describe "Ruby Arrays" do
     expect( array ).to eq( [1] )
 
     array[1] = 2
-    expect( array ).to eq( [1, __] )
+    expect( array ).to eq( [1, 2] )
 
     array << 333
-    expect( array ).to eq( __ )
+    expect( array ).to eq( [1, 2, 333] )
   end
 
   it "provides the subscript operator for accessing elements" do
     array = ["New York", "Paris", "London", "Milan"]
 
-    expect( array[0] ).to eq( __ )
-    expect( array[3] ).to eq( __ )
+    expect( array[0] ).to eq( "New York" )
+    expect( array[3] ).to eq( "Milan" )
 
     # What do you thing will happen here?
-    expect( array[-1] ).to eq( __ )
-    expect( array[-3] ).to eq( __ )
+    expect( array[-1] ).to eq( "Milan" )
+    expect( array[-3] ).to eq( "Paris" )
   end
 
   it "has methods for accessing the beginning and end of the array" do
     array = ["New York", "Paris", "London", "Milan"]
 
-    expect( array.first ).to eq( __ )
-    expect( array.last  ).to eq( __ )
+    expect( array.first ).to eq( "New York" )
+    expect( array.last  ).to eq( "Milan" )
   end
 
   it "can be sliced" do
     array = ["New York", "Paris", "London", "Milan"]
 
-    expect( array.slice(0,1) ).to eq( __ )
-    expect( array.slice(0,2) ).to eq( __ )
-    expect( array.slice(3,3) ).to eq( __ )
-    expect( array.slice(2,20) ).to eq( __ )
-    expect( array.slice(4,0) ).to eq( __ )
-    expect( array.slice(4,10) ).to eq( __ )
-    expect( array.slice(5,0) ).to eq( __ )
+    expect( array.slice(0,1) ).to eq( ["New York"] )
+    expect( array.slice(0,2) ).to eq( ["New York", "Paris"] )
+    expect( array.slice(3,3) ).to eq( ["Milan"] )
+    expect( array.slice(2,20) ).to eq( ["London", "Milan"] )
+    expect( array.slice(4,0) ).to eq( [] )
+    expect( array.slice(4,10) ).to eq( [] )
+    expect( array.slice(5,0) ).to eq( nil )
   end
 
   it "is similar to a range" do
-    expect( (1..5).class ).to eq( __ )
+    expect( (1..5).class ).to eq( Range )
     expect( [1,2,3,4,5] ).not_to eq( (1..5) )
-    expect( __ ).to eq( (1..5).to_a )
-    expect( __ ).to eq( (1...5).to_a )
+    expect( [1,2,3,4,5] ).to eq( (1..5).to_a )
+    expect( [1,2,3,4] ).to eq( (1...5).to_a )
   end
 
   it "can be sliced by a range" do
     array = ["New York", "Paris", "London", "Milan"]
 
-    expect( __ ).to eq array[0..2]
-    expect( __ ).to eq array[0...2]
-    expect( __ ).to eq array[2..-1]
-    expect( __ ).to eq array[-2..1]
+    expect( ["New York", "Paris", "London"] ).to eq array[0..2]
+    expect( ["New York", "Paris"] ).to eq array[0...2]
+    expect( ["London", "Milan"] ).to eq array[2..-1]
+    expect( [] ).to eq array[-2..1]
   end
 
   it "can be used as a stack" do
@@ -70,11 +70,11 @@ RSpec.describe "Ruby Arrays" do
 
     array.push("on the end")
 
-    expect( array ).to eq( __ )
+    expect( array ).to eq( [1,2,"on the end"] )
 
     value = array.pop
-    expect( value ).to eq( __ )
-    expect( array ).to eq( __ )
+    expect( value ).to eq( "on the end" )
+    expect( array ).to eq( [1,2] )
   end
 
   it "can be used as a queue" do
@@ -82,13 +82,18 @@ RSpec.describe "Ruby Arrays" do
     array.push(:third)
 
     value = array.shift
-    expect( value ).to eq( __ )
-    expect( array ).to eq( __ )
+    expect( value ).to eq( :first )
+    expect( array ).to eq( [:second, :third] )
 
     # Sometimes you need to get things to the front of a queue
     array.unshift(value)
 
-    expect( array ).to eq( __ )
+    expect( array ).to eq( [:first, :second, :third] )
+  end
+
+  it "contains first few elements" do
+    array = [1,2,3]
+    expect( array.first(2) ).to eq( [1,2] )
   end
 
 end
