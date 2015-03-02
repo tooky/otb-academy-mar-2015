5b904a72d03c22703be7aceb9fe18cc11b997338
Christopher Rigby <christopher.rigby@onthebeach.co.uk>
2015-03-02 10:13:15 +0000

    Finished test

--- the-language/spec/introduction_spec.rb
+++ the-language/spec/introduction_spec.rb
@@ -3,25 +3,25 @@ RSpec.describe "Learning ruby" do
   context "using expectations" do
 
     it "checks reality" do
-      expect( true ).to be false # it should be true
+      expect( true ).to be true # it should be true
     end
 
     it "verifies our expectations against reality" do
-      expected = __
+      expected = 2
       actual   = 1 + 1
 
       expect(actual).to be expected
     end
 
     it "has better ways to compare equality" do
-      expected = __
+      expected = 2
       actual   = 1 + 1
 
       expect(actual).to eq expected
     end
 
     it "will ask you to fill in values" do
-      expect( 1 + 1 ).to eq __
+      expect( 1 + 1 ).to eq 2
     end
 
   end
