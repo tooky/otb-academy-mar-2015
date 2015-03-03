022ba061b485f585dbad1bfe90f5c5c42de1a245
Christopher Rigby <christopher.rigby@onthebeach.co.uk>
2015-03-03 11:31:34 +0000

    Lol triangle

--- the-language/lib/triangle.rb
+++ the-language/lib/triangle.rb
@@ -15,6 +15,13 @@
 #
 def triangle(a, b, c)
   # WRITE THIS CODE
+  if a == b && b == c then
+    :equilateral
+  elsif a == b || b == c || c == a then
+    :isosceles
+  else
+    :scalene
+  end
 end
 
 # Error class used in part 2.  No need to change this code.
