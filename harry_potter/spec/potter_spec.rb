7166c98790f6d61cbf3887f9263cc24097391cae
Duncan Smith <duncan.smith@onthebeach.co.uk>
2015-03-06 03:27:59 +0000

    First test added - 1 book at full price

--- harry_potter/spec/potter_spec.rb
+++ harry_potter/spec/potter_spec.rb
@@ -1,3 +1,10 @@
-Rspec.describe "A purchase of Harry Potter Books" do
+RSpec.describe "A purchase of Harry Potter Books" do
+  context "Basket" do
+    it "contains 1 unique book" do
+      let(:books)  { ["book1"] }
+      let(:basket) { Basket.new(books) }
 
+      expect(basket.total).to eq(8)
+    end
+  end
 end
