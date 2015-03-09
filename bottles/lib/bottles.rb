0a200eb0e07a1d4011c8c026eedb8270e2cf2b29
Tom Clark <tom.clark@onthebeach.co.uk>
2015-03-09 15:57:53 +0000

    Removed comparison of -1

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -16,14 +16,6 @@ class BottleNumber
     if @number_of_bottles == 0
       "no more"
     else
-      bottle_left_number
-    end
-  end
-  
-  def bottle_left_number
-    if @number_of_bottles == -1
-      99
-    else
       @number_of_bottles.to_s
     end
   end
@@ -44,12 +36,20 @@ class BottleNumber
     end
   end
   
+  def next_bottle_count
+    if @number_of_bottles == 0
+      99
+    else
+      @number_of_bottles - 1
+    end
+  end
+  
   def bottles_left_count
-    BottleNumber.new(@number_of_bottles - 1).count_bottles
+    BottleNumber.new(next_bottle_count).count_bottles
   end
   
   def bottles_left_container
-    BottleNumber.new(@number_of_bottles - 1).container
+    BottleNumber.new(next_bottle_count).container
   end
   
 end
