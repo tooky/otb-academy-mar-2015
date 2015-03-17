ea85624568e545ff1aeda65662debbfcef7d7e99
Steve Tooke <steve@boxjump.co.uk>
2015-03-17 13:49:04 +0000

    Remove person instance variables

--- shouty/features/step_definitions/steps.rb
+++ shouty/features/step_definitions/steps.rb
@@ -15,30 +15,26 @@ Given(/^the following geolocations:$/) do |locations|
   end
 end
 
-Given(/^James is at "(.*?)"$/) do |location|
-  @james = Person.new(the_network)
-  @james.location = @locations[location]
-end
-
-Given(/^Chris is at "(.*?)"$/) do |location|
-  @chris = Person.new(the_network)
-  @chris.location = @locations[location]
+Given(/^(\w+) is at "(.*?)"$/) do |name, location|
+  @people ||= {}
+  @people[name] = Person.new(the_network)
+  @people[name].location = @locations[location]
 end
 
 When(/^Chris shouts "(.*?)"$/) do |message|
   @the_shout = message
-  @chris.shout(@the_shout)
+  @people["Chris"].shout(@the_shout)
 end
 
 When(/^Chris shouts:$/) do |message|
   @the_shout = message
-  @chris.shout(@the_shout)
+  @people["Chris"].shout(@the_shout)
 end
 
 Then(/^James doesn't hear Chris' shout$/) do
-  expect(@james.heard_messages).not_to include(@the_shout)
+  expect(@people["James"].heard_messages).not_to include(@the_shout)
 end
 
 Then(/^James hears Chris' shout$/) do
-  expect(@james.heard_messages).to include(@the_shout)
+  expect(@people["James"].heard_messages).to include(@the_shout)
 end
